e6fa79d52508af2ed95513ac67d804642793a687
Leopoldo Santos <santosg572@gmail.com>
2017-08-20 15:22:53 -0500

    message

--- boxplot_V2.py
+++ boxplot_V2.py
@@ -1,20 +1,13 @@
-## numpy is used for creating fake data
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-## Create data
 np.random.seed(10)
-data_to_plot = np.random.normal(100, 10, 200)
+datos = np.random.normal(100, 10, 200)
 
-# Create a figure instance
-fig = plt.figure(1, figsize=(9, 6))
+plt.figure(1, figsize=(9, 6))
 
-# Create an axes instance
-ax = fig.add_subplot(111)
+plt.boxplot(datos)
+plt.ylim([50,150])
 
-# Create the boxplot
-bp = ax.boxplot(data_to_plot)
-ax.set_ylim([0,200])
-
-fig.show()
+plt.show()
 
